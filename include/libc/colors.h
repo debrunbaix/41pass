868fe855f9a0e1d9ca9dff21ce54656fec6f69d8
Wyshteria <34022437+Wyshteria@users.noreply.github.com>
2025-09-09 10:28:02 +0200

    [ADD] colors.h header file with color definitions

---
+++ include/libc/colors.h
@@ -0,0 +1,37 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+// Reset
+#define COLOR_RESET   "\x1b[0m"
+
+// Regular Colors
+#define COLOR_BLACK   "\x1b[30m"
+#define COLOR_RED     "\x1b[31m"
+#define COLOR_GREEN   "\x1b[32m"
+#define COLOR_YELLOW  "\x1b[33m"
+#define COLOR_BLUE    "\x1b[34m"
+#define COLOR_MAGENTA "\x1b[35m"
+#define COLOR_CYAN    "\x1b[36m"
+#define COLOR_WHITE   "\x1b[37m"
+
+// Bold Colors
+#define COLOR_BBLACK   "\x1b[1;30m"
+#define COLOR_BRED     "\x1b[1;31m"
+#define COLOR_BGREEN   "\x1b[1;32m"
+#define COLOR_BYELLOW  "\x1b[1;33m"
+#define COLOR_BBLUE    "\x1b[1;34m"
+#define COLOR_BMAGENTA "\x1b[1;35m"
+#define COLOR_BCYAN    "\x1b[1;36m"
+#define COLOR_BWHITE   "\x1b[1;37m"
+
+// Background Colors
+#define BGCOLOR_BLACK   "\x1b[40m"
+#define BGCOLOR_RED     "\x1b[41m"
+#define BGCOLOR_GREEN   "\x1b[42m"
+#define BGCOLOR_YELLOW  "\x1b[43m"
+#define BGCOLOR_BLUE    "\x1b[44m"
+#define BGCOLOR_MAGENTA "\x1b[45m"
+#define BGCOLOR_CYAN    "\x1b[46m"
+#define BGCOLOR_WHITE   "\x1b[47m"
+
+#endif // COLORS_H
